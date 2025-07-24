c6f0633cf969f0c87f6a896f78497e320793f758
wagaane <ablayefaye9725@gmail.com>
2025-07-24 11:57:59 +0000

    commit initialize project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+      <div className={'container'}>
+        hello <button className={'btn btn-danger'}></button>
+      </div>
+  );
+}
+
+export default App;
